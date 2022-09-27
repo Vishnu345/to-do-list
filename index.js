85d21de513db07d9c5f3f04511bd497ea61d25da
VISHNU KOLLURI <70359344+Vishnu345@users.noreply.github.com>
2022-09-27 13:24:23 +0530

    Update and rename todo.js to index.js

--- index.js
+++ index.js
@@ -21,38 +21,27 @@ const todoList = () => {
   };
 
   const overdue = () => {
-    let overdue_list = all.filter(function (item) {
-      return item.dueDate === yesterday;
-    });
+    let overdue_list = all.filter((item) => item.dueDate === yesterday);
     return overdue_list;
   };
 
   const dueToday = () => {
-    let dueToday_list = all.filter(function (item) {
-      return item.dueDate === today;
-    });
+    let dueToday_list = all.filter((item) => item.dueDate === today);
     return dueToday_list;
   };
 
   const dueLater = () => {
-    let dueLater_list = all.filter(function (item) {
-      return item.dueDate === tomorrow;
-    });
+    let dueLater_list = all.filter((item) => item.dueDate === tomorrow);
     return dueLater_list;
   };
 
   const toDisplayableList = (list) => {
-    let display_list = list.map(function (item) {
-      if (item["dueDate"] === today && item["completed"] === false) {
-        return "[ ]" + " " + String(item["title"]);
-      } else if (item["dueDate"] === today && item["completed"] === true) {
-        return "[x]" + " " + String(item["title"]);
-      } else {
-        return "[ ]" + " " + String(item["title"]) + " " + String(item["dueDate"]);
-      }
+    let display_list = list.map((item) => {
+      let completionStatus = item.completed ? "[x]": "[ ]";
+      let displayedDate = item.dueDate === today ? "" : item.dueDate;
+      return `${completionStatus} ${item.title} ${displayedDate}`.trim();
     });
     let output_string = display_list.join("\n");
-
     return output_string;
   };
 
